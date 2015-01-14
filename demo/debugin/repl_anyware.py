e88d79040bb067ee06c49898a84042b4fd6cb628
Ægir Örn Símonarson <agir@mbl.is>
2015-01-14 10:33:38 +0000

    Adding REPL anyware code.

---
+++ demo/debugin/repl_anyware.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+This is the code you nead to open upp a Pythong REPL from withe inn youre running applicaiton.
+"""
+from IPython import embed
+
+variable = 'String Value'
+embed()  # programs should pause hear and open up the REPL.
+print variable
